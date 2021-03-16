5a93d906ca04df09bd1ea37fd737f310e54092e3
Attila Bertók <bertok.attila@edu.bme.hu>
2021-03-16 15:25:49 +0100

    jav

--- Skeleton/Skeleton.cpp
+++ Skeleton/Skeleton.cpp
@@ -376,6 +376,34 @@ public:
 			}
 		}
 	}
+	void move(vec3 hyperbolicMotionVector) {
+		float hyperbolicMotionVectorLength = hyperbolicDistance(hyperbolicMotionVector, vec3(0, 0, 1)); // az eltolas hossza
+		vec3 hyperbolicMotionVectorDirection = (hyperbolicMotionVector - vec3(0, 0, 1) * cosh(hyperbolicMotionVectorLength)) / sinh(hyperbolicMotionVectorLength); /// a hiperbola origojaban ervenyes iranyvektor
+
+		// ketto tukrozesi pont valasztasa az hiperbolikus szakasz egyenlete alapjan, fontos, hogy a ketto kozotti tavolsag fele legyen a kivant eltolas tavolsaganak
+		vec3 m1 = vec3(0, 0, 1);
+		vec3 m2 = vec3(0, 0, 1) * cosh(hyperbolicMotionVectorLength * 0.50) + hyperbolicMotionVectorDirection * sinh(hyperbolicMotionVectorLength * 0.50);
+
+		for (int i = 0; i < numberOfPoints; i++)
+		{
+			// pont tukrozese m1-re
+			float m1Distance = hyperbolicDistance(m1, hyperbolicPoints[i]); // a tavolsag a pont es m1 kozott
+
+			vec3 m1Direction = (m1 - hyperbolicPoints[i] * cosh(m1Distance)) / sinh(m1Distance); // az adott pontban ervenyes iranyvektor az m1 pont fele
+			vec3 pointM1Mirror = hyperbolicPoints[i] * cosh(2 * m1Distance) + m1Direction * sinh(2 * m1Distance);
+
+			// m1-re tukrozott pont tukrozese m2-re
+			float m2Distance = hyperbolicDistance(m2, pointM1Mirror);
+			vec3 m2Direction = (m2 - pointM1Mirror * cosh(m2Distance)) / sinh(m2Distance);
+			vec3 pointM2Mirror = pointM1Mirror * cosh(2 * m2Distance) + m2Direction * sinh(2 * m2Distance);
+
+			hyperbolicPoints[i] = pointM2Mirror; // az igy kapott pont lesz az uj pozicioja
+
+		}
+		refreshDescartesFromHyperbolic();
+		refreshEdgeCoordinates(hyperbolicPoints, edgeCoordinates, hyperbolicEdgeCoordinates);
+		draw();
+	}
 };
 
 Graph graph;
@@ -425,34 +453,7 @@ void onMouseMotion(int pX, int pY) {	// pX, pY are the pixel coordinates of the 
 	if (abs(descartesMotionVector.x) <= 0.0005 || abs(descartesMotionVector.y) <= 0.0005) {/// kis eltolasnal elszallnanak a pontok, szerintem azert mert pontatlan a float
 		return;									///  ezert ilyen kis eltolast nem engedek meg
  	}
-
-	vec3 hyperbolicMotionVector = graph.descartesToHyperbolic(descartesMotionVector); // az eltolas vektor hiperbolikus geometriaban
-	float hyperbolicMotionVectorLength = graph.hyperbolicDistance(hyperbolicMotionVector, vec3(0, 0, 1)); // az eltolas hossza
-	vec3 hyperbolicMotionVectorDirection = (hyperbolicMotionVector - vec3(0, 0, 1) * cosh(hyperbolicMotionVectorLength)) / sinh(hyperbolicMotionVectorLength); /// a hiperbola origojaban ervenyes iranyvektor
-	
-	// ketto tukrozesi pont valasztasa az hiperbolikus szakasz egyenlete alapjan, fontos, hogy a ketto kozotti tavolsag fele legyen a kivant eltolas tavolsaganak
-	vec3 m1 = vec3(0, 0, 1);
-	vec3 m2= vec3(0, 0, 1) * cosh(hyperbolicMotionVectorLength * 0.50) + hyperbolicMotionVectorDirection * sinh(hyperbolicMotionVectorLength * 0.50);
-
-	for (int i = 0; i < numberOfPoints; i++)
-	{
-		// pont tukrozese m1-re
-		float m1Distance = graph.hyperbolicDistance(m1, graph.hyperbolicPoints[i]); // a tavolsag a pont es m1 kozott
-
-		vec3 m1Direction = (m1 - graph.hyperbolicPoints[i] * cosh(m1Distance)) / sinh(m1Distance); // az adott pontban ervenyes iranyvektor az m1 pont fele
-		vec3 pointM1Mirror = graph.hyperbolicPoints[i] * cosh(2 * m1Distance) + m1Direction * sinh(2 * m1Distance);
-
-		// m1-re tukrozott pont tukrozese m2-re
-		float m2Distance = graph.hyperbolicDistance(m2, pointM1Mirror);
-		vec3 m2Direction = (m2 - pointM1Mirror * cosh(m2Distance)) / sinh(m2Distance);
-		vec3 pointM2Mirror = pointM1Mirror * cosh(2 * m2Distance) + m2Direction * sinh(2 * m2Distance);
-
-		graph.hyperbolicPoints[i] = pointM2Mirror; // az igy kapott pont lesz az uj pozicioja
-		
-	}
-	graph.refreshDescartesFromHyperbolic();
-	graph.refreshEdgeCoordinates(graph.hyperbolicPoints, graph.edgeCoordinates, graph.hyperbolicEdgeCoordinates);
-	graph.draw();
+	graph.move(graph.descartesToHyperbolic(descartesMotionVector)); // az eltolasvektor hierbolikus megfelelojevel tortenik az eltolas
 	motionStartCoordinates = motionEndCoordinates; // a kovetkezo lefutasnal a mar megtortent eltolast ne csinalja meg ujra
 
 }
